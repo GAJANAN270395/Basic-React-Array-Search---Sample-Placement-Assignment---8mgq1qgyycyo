6b4b889cb2a4feaac5db8e1cfd1a2c869e9ea5f7
GAJANAN270395 <108356887+GAJANAN270395@users.noreply.github.com>
2022-11-03 09:53:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,34 @@ import '../styles/App.css';
 
 const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
 const App = () => {
+ const [array, SetArray] = useState("");
 
+  const search = (event) => {
+    console.log(event.target.value);
+    SetArray(event.target.value);
+  }
   
   return (
     <div id="main">
-      
+      <h1>Search</h1>
+      <input type="text" id="search-input" onChange={search} />
+      <h1>Result</h1>
+      <ul>
+        {
+          searchArray.filter((item) => {
+            const searchValue = array.toLowerCase()
+            const arrayItem = item.toLowerCase()
+
+            return (searchValue && item.includes(searchValue) && searchValue !== arrayItem)
+
+          }
+          )
+
+            .map((item, index) => (
+              <li key={index}>{item}</li>
+            ))
+        }
+      </ul>
     </div>
   )
 }
